0ed675c50d65178a2259c6aac8d278ccf0ba72c7
HouKaide <hordonvi@gmail.com>
2026-06-08 00:44:25 +0200

    Initial Commit

---
+++ src/etls/base_etl.py
@@ -0,0 +1,31 @@
+import os
+from platform import platform
+from typing import Union
+
+import pandas as pd
+
+
+class BaseETL:
+
+    def __init__(self):
+        self.spacing_bar = self.spacing_bar()
+
+    @property
+    def input_path(self) -> Union[str, NotImplementedError]:
+        return NotImplementedError("Method convert must be implemented")
+
+    @property
+    def destination_path(self) -> str:
+        NotImplementedError("Method convert must be implemented")
+
+    @staticmethod
+    def spacing_bar() -> str:
+        return r'\\' if platform == 'Windows' else "/"
+
+    def _store_datatype_parquet(self, df: pd.DataFrame, filename: str, folder_name: str):
+        final_path = f"{self.destination_path}{self.spacing_bar}{folder_name}{self.spacing_bar}"
+        if not os.path.exists(final_path):
+            os.makedirs(final_path)
+        df.to_parquet(f"{final_path}{self.spacing_bar}{os.path.split(filename)[1].replace('.csv', '')}.parquet",
+                      engine='fastparquet', index=False)
+
